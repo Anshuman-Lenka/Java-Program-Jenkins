60aa60f7c07e8c0b8c56063fd1ea10663a7a74f6
Anshuman Lenka <anshumanlenka7@gmail.com>
2024-05-05 15:41:49 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main{
+
+    public static void main(String[] args){
+
+        System.out.println("This is java program");
+        System.out.println("Software Engineer");
+        //System.out.println(new Date());
+
+
+    }
+}
