07f83fde601a9d48b97b3afd1c23bdec978c3311
Anshuman Lenka <anshumanlenka7@gmail.com>
2024-05-05 15:54:52 +0530

    Added student class

--- Main.java
+++ Main.java
@@ -1,12 +1,16 @@
+import java.util.Date;
+
 public class Main{
 
     public static void main(String[] args){
 
         System.out.println("This is java program");
         System.out.println("Software Engineer");
-        //System.out.println(new Date());
+        System.out.println(new Date());
         System.out.println("Hello....");
         System.out.println("I can do it....");
+        Student student= new Student();
+        student.sayHello();
 
 
 
